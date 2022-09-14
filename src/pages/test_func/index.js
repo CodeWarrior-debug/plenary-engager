0dbff5181091b45be77931220771c1d3ef7cb82a
James Jordan <jpjordan1987@gmail.com>
2022-09-14 03:09:46 -0400

    working sporadically

--- src/pages/test_func/index.js
+++ src/pages/test_func/index.js
@@ -6,15 +6,21 @@ import DivTest from "../components/div_test";
 
 export default function TestFunc() {
   const booksArray = require("../../assets/books_w_chapters.json");
-  const verseCountArr=require("../../assets/book_chapters_w_versecounts.json");
+  const verseCount=require("../../assets/book_chapters_w_versecounts.json");
   const [verse, setVerse]=useState("");
   const [chapters, setChapters] = useState("");
+  const [chapter, setChapter] = useState("");
   const [book, setBook] = useState("");
   const [label, setLabel] = useState("");
   const [chaptersArr, setChaptersArr] = useState([]);
   const [verseArr, setVerseArr] = useState([]);
   const [stampChap, setStampChap] = useState("");
-  const [stampVerse, setStampVerse] = useState("");
+  const [bookDashChap, setBookDashChap] = useState("");
+  // const [stampVerse, setStampVerse] = useState("");
+
+  useEffect(() => {
+    console.log(verseCount[label + '-' + stampChap]);
+  },[])
 
   let updateChapters = (e) => {
     setChapters(e.chapter_count);
@@ -28,34 +34,41 @@ export default function TestFunc() {
   };
 
   let stampVerses = (e) => {
-    setStampVerse(e.label);
+    setVerse(e.label);
   };
+  
+    const populateChapters = (max) => {
+      let chapArr = [];
+      for (var i = 1; i <= max; i++) {
+        chapArr.push({ label: i }); //label has to be an object
+      }
+      setChaptersArr(chapArr);
+    };
+  
+    const populateVerses = (max) => {
+      let verseArr = [];
+      for (var i = 1; i <= max; i++) {
+        verseArr.push({ label: i }); //label has to be an object
+      }
+      setVerseArr(verseArr);
+    };
+
+  const evalBookChap=()=>{
+    
+    setBookDashChap(label + '-' + stampChap)
+    console.log('bookdashchap is' + bookDashChap)
+    console.log(verseCount.bookDashChap)
+  }
 
   const BookSelect = () => (
-    <Select options={booksArray} onChange={updateChapters} />
+    <Select options={booksArray} onChange= {(e) =>{ updateChapters(e); evalBookChap();}} />
   );
 
-  const populateChapters = (max) => {
-    let chapArr = [];
-    for (var i = 1; i <= max; i++) {
-      chapArr.push({ label: i }); //has to be an object
-    }
-    setChaptersArr(chapArr);
-  };
-
-  const populateVerses = (max) => {
-    let verseArr = [];
-    for (var i = 1; i <= max; i++) {
-      verseArr.push({ label: i }); //has to be an object
-    }
-    setVerseArr(verseArr);
-  };
-
-  const ChapterSelect = (chapters) => (
-    <Select options={chaptersArr} onChange={stampChapters} />
+  const ChapterSelect = () => (
+    <Select options={chaptersArr} onChange={(e) => {stampChapters(e); evalBookChap();}} />
   );
 
-  const VerseSelect = (chapters) => (
+  const VerseSelect = () => (
     <Select options={verseArr} onChange={stampVerses} />
   );
 
@@ -64,12 +77,12 @@ export default function TestFunc() {
       <h1>Choose A Range of Verses</h1>
       <BookSelect />
       <ChapterSelect chapters={chapters} />
-      <ChapterSelect verses={verses} />
+      <VerseSelect verse={verse} />
       <DivTest number={label} />
       <DivTest number={book} />
       <DivTest number={chapters} />
       <DivTest number={stampChap} />
-      <DivTest number={stampVerse} />
+      <DivTest number={verse} />
     </>
   );
 }
