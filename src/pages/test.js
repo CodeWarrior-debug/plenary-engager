879cbddfdd61c76d102a0e977efbba878af87164
CodeWarrior-debug <jpjordan1987@gmail.com>
2022-09-19 20:57:44 -0400

    two tables solution

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./styles.css"
 
 export default function Test() {
 
-    const mytab1 = document.getElementById('mytab1')
+    const mytab1 = document.getElementById('originalsTab1')
 
     const storeItems = () => {
 
@@ -13,13 +14,10 @@ export default function Test() {
         {
            let val = cell.innerText; // your code below
            storeArr.push(val);
+           console.log(row);
         }
     }
     console.log(storeArr);
-}
-
-const Button = () => {
-    <button style={{"min-height":"100px"}} type='button' onClick={storeItems}>Swap Items</button>
 }
 
   return (
@@ -28,13 +26,28 @@ const Button = () => {
     <button onClick={storeItems}>
         Store Verses
       </button>
+<div class="wrapper">
 
-      <table id="mytab1">
+      <table id="originalsTab1">
   <thead>
     <tr>
       <th>header1</th>
-      <th>header2</th>
-      <th>header3</th>
+    </tr>
+   </thead>
+   <tbody>
+     <tr>
+       <td>text1.1</td>
+       <td>text1.2</td>
+       <td>text1.3</td>
+     </tr>
+  </tbody>
+</table>
+
+
+<table id="replaceTab1">
+  <thead>
+    <tr>
+      <th>header1</th>
     </tr>
    </thead>
    <tbody>
@@ -44,19 +57,10 @@ const Button = () => {
        <td>text1.3</td>
      </tr>
      <tr>
-       <td>text2.1</td>
-       <td>text2.2</td>
-       <td>text2.3</td>
-     </tr>
-     <tr>
-       <td>text3.1</td>
-       <td>text3.2</td>
-       <td>text3.3</td>
-     </tr>
-     <tr>
      </tr>
   </tbody>
 </table>
+</div>
 </>
   )
 }
